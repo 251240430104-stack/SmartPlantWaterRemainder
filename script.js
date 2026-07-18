cedc208367b4a1d121694114cd8c251abf83a2b9
251240430104-stack <251240430104@srcas.ac.in>
2026-07-18 17:32:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Notification Permission
+if ("Notification" in window) {
+    Notification.requestPermission();
+}
+
 function saveReminder() {
 
     let plant = document.getElementById("plant").value;
@@ -19,14 +24,39 @@ function saveReminder() {
 
     list.appendChild(li);
 
+    // Notification Time Check
+    let now = new Date();
+    let reminderTime = new Date();
+
+    let parts = time.split(":");
+    reminderTime.setHours(parts[0]);
+    reminderTime.setMinutes(parts[1]);
+    reminderTime.setSeconds(0);
+
+    let delay = reminderTime - now;
+
+    if (delay > 0) {
+        setTimeout(() => {
+            if (Notification.permission === "granted") {
+                new Notification("🌱 Water Reminder", {
+                    body: "Time to water " + plant + " 💧"
+                });
+            } else {
+                alert("🌱 Time to water " + plant + " 💧");
+            }
+        }, delay);
+    }
+
     document.getElementById("plant").value = "";
     document.getElementById("time").value = "";
 
     alert("Reminder Saved Successfully!");
 }
-function deleteReminder(button){
+
+function deleteReminder(button) {
     button.parentElement.remove();
 }
+
 function showTime() {
     let now = new Date();
     document.getElementById("clock").innerHTML =
@@ -34,4 +64,3 @@ function showTime() {
 }
 
 setInterval(showTime, 1000);
-
